b95c52b9273fa8360a2697d1df60b96423eb00c4
Jack Scott <jack.scott10@gmail.com>
2024-03-30 18:28:38 +0000

    Add files to package json

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve("src", "index.tsx"),
-      name: "boids-canvas",
+      entry: resolve("src", "index.tsx"),
+      name: "jdls-boids-canvas",
       fileName: (format) => `boids-canvas.${format}.js`,
     },
     rollupOptions: {
